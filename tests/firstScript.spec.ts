c84a1a7cd54afc1b34b2a2d619c7bfbeddb9f1c6
MahbubulRishad <mahbubulrishad@gmail.com>
2025-10-07 20:21:28 +0600

    Playwright with Typescript kick start

---
+++ tests/firstScript.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test';
+
+
+// fixture - global variable : page, browser
+test("Verify page tittle", async ({ page }) => {
+    await page.goto("http://www.automationpractice.pl/index.php");
+
+    let title: string = await page.title();
+    console.log("Title", title);
+
+    await expect(page).toHaveTitle("My Shop");
+});
