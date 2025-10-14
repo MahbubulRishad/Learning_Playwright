85507ba26d2d5d272dd6be8e5429c7e4f02a3a29
MahbubulRishad <mahbubulrishad@gmail.com>
2025-10-15 01:43:00 +0600

    getByRole() - method

--- tests/Built-In-Locators/built-In-Locator.spec.ts
+++ tests/Built-In-Locators/built-In-Locator.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect, Locator } from "@playwright/test";
 
 
-// page.getByAltText() - use this locator when supports alt text such as img or area elemenents
+// 1. page.getByAltText() - use this locator when supports alt text such as img or area elemenents
 test("Verify Logo is visible - getByAltText()", async ({ page }) => {
 
     await page.goto("https://demo.nopcommerce.com/");
@@ -11,7 +11,7 @@ test("Verify Logo is visible - getByAltText()", async ({ page }) => {
 });
 
 
-// page.getByText() - find an element by the text it contains, can match substring or exact string which have noting any action
+// 2. page.getByText() - find an element by the text it contains, can match substring or exact string which have noting any action
 // locate by visible text
 // use this locator to find non interactive elements like div, span, package. etc..
 test("Verify the Welcome text - getByText()", async ({ page }) => {
@@ -24,6 +24,19 @@ test("Verify the Welcome text - getByText()", async ({ page }) => {
     await expect(page.getByText("Welcome to our store")).toBeVisible(); // supportted substring, regular expression
 
     await expect(page.getByText(/Welcome\s+To\s+Our\s+store/i)).toBeVisible(); // Regular expression
+});
+
+
+// 3. page.getByRole() - (Interactable) - include buttons, checkbox, headings, links, lists, table and many more and 
+//    follow W3C specifications for ARIA role
+test("Verify the Register link is workable", async ({ page }) => {
+    await page.goto("https://demo.nopcommerce.com/");
+
+    const registerL: Locator = page.getByRole("link", { name: "Register" });
+    await registerL.click();
+
+    const registerFormHeader: Locator = page.getByRole("heading", {name: "Register"});
+    await expect(registerFormHeader).toBeVisible();
 });
 
 
