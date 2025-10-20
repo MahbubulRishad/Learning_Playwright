5d0532512c3f6765bb68aa8510b2fbd8ba3408cd
MahbubulRishad <mahbubulrishad@gmail.com>
2025-10-21 02:04:37 +0600

    Xpath

--- tests/Xpath/xpath.spec.ts
+++ tests/Xpath/xpath.spec.ts
@@ -16,16 +16,16 @@ test("Verify Logo", async ({ page }) => {
 
     // console.log(productsTitleEl.textContent()); // error: strict mode violation
 
-    const textFirstItem: (string | null) =  await productsTitleEl.first().textContent();
+    const textFirstItem: (string | null) = await productsTitleEl.first().textContent();
     console.log("First item trimmed:", textFirstItem?.trim());
     console.log("Taking last element:", await productsTitleEl.last().textContent());
     console.log("Taking n-th element:", await productsTitleEl.nth(3).textContent()); // 3rd element
 
     const allProductsList: string[] = await productsTitleEl.allTextContents();
-    const trimedProducts =  allProductsList.map(item => item.trim());
+    const trimedProducts = allProductsList.map(item => item.trim());
     console.log("All Products list with trimed:", trimedProducts);
 
-    for (let product of trimedProducts){
+    for (let product of trimedProducts) {
         console.log("Product Title: ", product);
     }
 
